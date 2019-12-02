399f733ef48ca28d5000d22a405320dc03f20196
Pixelastic <tim@pixelastic.com>
2019-12-02 13:47:42 +0100

    feat(lint): Allow importing aberlaas/ from templates

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,21 @@
 /* eslint-disable import/no-commonjs */
 module.exports = {
   extends: ['./lib/configs/eslint.js'],
+  overrides: [
+    {
+      files: ['./templates/*.js'],
+      rules: {
+        // Allow importing files from aberlaas/ in templates
+        // It is expected to fail in dev, but will correctly resolve once copied
+        // to the host project
+        'import/no-unresolved': [
+          'error',
+          {
+            commonjs: true,
+            ignore: ['^aberlaas/'],
+          },
+        ],
+      },
+    },
+  ],
 };
