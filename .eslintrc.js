4fcf667560fa3100a65ab8128c89a88ec00c4293
Pixelastic <tim@pixelastic.com>
2020-04-11 22:28:56 +0200

    feat(lint): Add node linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,21 @@
+const path = require('path');
+const parentDir = path.resolve('..');
 module.exports = {
   extends: ['./lib/configs/eslint.js'],
   overrides: [
     {
       files: ['./templates/*.js'],
       rules: {
-        // Allow importing files from aberlaas/ in templates
-        // It is expected to fail in dev, but will correctly resolve once copied
-        // to the host project
-        'import/no-unresolved': [
+        // Files in templates are trying to require('aberlaas/lib/*')
+        // This works when copied into the root of a project as expected, but
+        // not when inside the project itself as this path does not resolve
+        // We prevent ESLint from throwing a linting error by adding the parent
+        // directory of our local aberlaas code as one of the resolved path
+        // This expect the main directory to be named "aberlaas", though
+        'node/no-missing-require': [
           'error',
           {
-            commonjs: true,
-            ignore: ['^aberlaas/'],
+            resolvePaths: [parentDir],
           },
         ],
       },
