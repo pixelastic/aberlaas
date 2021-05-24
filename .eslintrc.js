5f90af5f8d1d8ca994b47355ed085acb534ab55b
Tim Carry <tim@pixelastic.com>
2021-05-24 22:13:30 +0200

    dev(lint): Disable rules in templates

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
         // This works when copied into the root of a project as expected, but
         // not when inside the project itself as this path does not resolve
         'node/no-missing-require': [0],
+        'node/no-extraneous-require': [0],
       },
     },
   ],
