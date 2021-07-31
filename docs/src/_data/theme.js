e822721cfcf7c13f2cfb1b395051f34e8d37eb6a
Tim Carry <tim@pixelastic.com>
2021-07-31 14:51:42 +0200

    docs: adding sidebar links

--- docs/src/_data/theme.js
+++ docs/src/_data/theme.js
@@ -9,13 +9,114 @@ module.exports = {
       links: [
         {
           title: 'Getting Started',
-          href: 'gettingStarted',
+          href: '/',
+        },
+        {
+          title: 'Configuration',
+          href: 'configuration',
         },
       ],
     },
     {
-      name: 'API',
-      links: ['init', 'read', 'write'],
+      name: 'Setup',
+      links: [
+        {
+          title: 'Overview',
+          href: 'setup/',
+        },
+        {
+          title: 'CircleCI',
+          href: 'setup/circleci',
+        },
+        {
+          title: 'Renovate',
+          href: 'setup/renovate',
+        },
+      ],
+    },
+    {
+      name: 'Lint',
+      links: [
+        {
+          title: 'Overview',
+          href: 'lint/',
+        },
+        {
+          title: 'Configuration',
+          href: 'test/configuration',
+        },
+        {
+          title: 'CSS',
+          href: 'lint/css',
+        },
+        {
+          title: 'JavaScript',
+          href: 'lint/js',
+        },
+        {
+          title: 'JSON',
+          href: 'lint/json',
+        },
+        {
+          title: 'YAML',
+          href: 'lint/yaml',
+        },
+        {
+          title: 'CircleCI',
+          href: 'lint/circleci',
+        },
+      ],
+    },
+    {
+      name: 'Test',
+      links: [
+        {
+          title: 'Overview',
+          href: 'test/',
+        },
+        {
+          title: 'Configuration',
+          href: 'test/configuration',
+        },
+        {
+          title: 'Jest',
+          href: 'test/jest',
+        },
+      ],
+    },
+    {
+      name: 'Release',
+      links: [
+        {
+          title: 'Overview',
+          href: 'release/',
+        },
+        {
+          title: 'Auto-release',
+          href: 'release/auto-release',
+        },
+      ],
+    },
+    {
+      name: 'Pre-commit',
+      links: [
+        {
+          title: 'Overview',
+          href: 'lint/',
+        },
+        {
+          title: 'Husky',
+          href: 'precommit/husky',
+        },
+        {
+          title: 'Lintstaged',
+          href: 'precommit/lintstaged',
+        },
+      ],
+    },
+    {
+      name: 'Other commands',
+      links: ['ci', 'compress', 'readme', 'setup'],
     },
   ],
 };
