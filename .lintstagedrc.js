ceae0152a70f76d751d61d7744ea2e91744a4826
Tim Carry <tim@pixelastic.com>
2024-01-17 16:09:51 +0100

    Move LintStage config to ESM

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,10 +1,4 @@
-// import config from './lib/configs/lintstaged.js';
-// console.info('root');
-// console.info(config);
-// export default config;
-
-// lint-staged.config.js
-export default (allStagedFiles) => {
-  console.info(allStagedFiles);
-  return ['eslint'];
+import config from './lib/configs/lintstaged.js';
+export default {
+  ...config,
 };
