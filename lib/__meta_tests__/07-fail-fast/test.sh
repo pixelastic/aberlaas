017d0bd89af3651ae1bc7b4d9eaaf712cb852b21
Tim Carry <tim@pixelastic.com>
2024-10-03 17:14:30 +0200

    Add meta test for --failFast

---
+++ lib/__meta_tests__/07-fail-fast/test.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+source "$(dirname "$0")/../test_framework/index"
+
+test "should fail early with --failFast"
+
+actual() {
+	"${ROOT}"/lib/bin/aberlaas.js test \
+		--failFast \
+		"${HERE}/__tests__/feature.js"
+}
+
+expect_to_fail &&
+	expect_to_contain "one: pass" &&
+	expect_to_contain "two: fail" &&
+	expect_not_to_contain "three: pass"
