d571a4a050cea1e4d2108502b74c9cc6874d0262
Tim Carry <tim@pixelastic.com>
2024-10-10 17:16:20 +0200

    tmp

--- modules/lib/bin/aberlaas.js
+++ modules/lib/bin/aberlaas.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 import aberlaas from '../lib/main.js';
 
-(async () => {
-  await aberlaas.run(process.argv.slice(2));
-})();
+console.info('in it');
+
+// (async () => {
+//   await aberlaas.run(process.argv.slice(2));
+// })();
