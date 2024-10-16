00f79f34d2a03c276437fdce6d7f17b5f34b39a9
Tim Carry <tim@pixelastic.com>
2024-10-16 17:53:33 +0200

    tmp

---
+++ scripts/meta/update.js
@@ -0,0 +1,36 @@
+/**
+ * Update a specific dependency to the latest version, for all modules that uses
+ * it
+ **/
+import path from 'path';
+import { _, pMap } from 'golgoth';
+import { glob, readJson, run } from 'firost';
+
+const dependenciesToUpdate = process.argv.slice(2);
+// const modules = await glob(['./package.json', './modules/*/package.json']);
+const modules = await glob('./modules/*/package.json');
+const concurrency = 1;
+
+await pMap(
+  modules,
+  async (module) => {
+    const thisModulePackageJson = await readJson(module);
+    const thisModuleDependencies = _.keys(thisModulePackageJson.dependencies);
+
+    await pMap(
+      dependenciesToUpdate,
+      async (dependencyToUpdate) => {
+        if (_.includes(thisModuleDependencies, dependencyToUpdate)) {
+          const thisModuleName = thisModulePackageJson.name;
+          const thisModulePath = path.dirname(module);
+          console.info(`Upgrading ${dependencyToUpdate} in ${thisModuleName}`);
+          await run(`yarn up --exact ${thisModuleName}`, {
+            cwd: thisModulePath,
+          });
+        }
+      },
+      { concurrency },
+    );
+  },
+  { concurrency },
+);
