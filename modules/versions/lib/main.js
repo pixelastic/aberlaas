de28314cf79534482e7c1f07c915b7bf16d595a5
Tim Carry <tim@pixelastic.com>
2025-10-02 11:33:38 +0200

    update version

--- modules/versions/lib/main.js
+++ modules/versions/lib/main.js
@@ -1,5 +1,5 @@
-export const nodeVersion = '18.18.0';
-export const yarnVersion = '4.6.0';
+export const nodeVersion = '22.18.0';
+export const yarnVersion = '4.10.3';
 export const norskaVersion = '2.9.0';
 export const norskaThemeDocsVersion = '5.0.3';
 export const lernaVersion = '4.0.0';
