223a30be7e17e250a3023b3003c5ea9b460bef4c
Tim Carry <tim@pixelastic.com>
2026-01-29 20:27:12 +0100

    feat(readme): add readme template system with auto-staging
    Introduces .README.template.md for generating project documentation
    and simplifies lintstaged config to auto-add generated files to git.

--- lintstaged.config.js
+++ lintstaged.config.js
@@ -2,13 +2,7 @@ import config from './modules/lib/configs/lintstaged.js';
 
 // Note: This is a local override when developing aberlaas itself. We can't call
 // yarn run aberlaas, so we need to manually call the ./lib/bin/aberlaas.js file
-const readmeCommands = [
-  './lib/bin/aberlaas.js readme',
-  'git add ./README.md ./lib/README.md',
-];
-
 export default {
   ...config,
-  'docs/src/**/*.md': readmeCommands,
-  '.github/README.template.md': readmeCommands,
+  '**/*.md': ['./modules/lib/bin/aberlaas.js readme --add-to-git'],
 };
