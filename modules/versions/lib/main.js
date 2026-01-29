6d8b0523d9a37e88f6b44b6229b6ce0dc4587009
Tim Carry <tim@pixelastic.com>
2026-01-29 10:35:09 +0100

    chore(deps): update Yarn to 4.12.0

--- modules/versions/lib/main.js
+++ modules/versions/lib/main.js
@@ -1,5 +1,5 @@
 export const nodeVersion = '22.18.0';
-export const yarnVersion = '4.10.3';
+export const yarnVersion = '4.12.0';
 export const norskaVersion = '2.9.0';
 export const norskaThemeDocsVersion = '5.0.3';
 
